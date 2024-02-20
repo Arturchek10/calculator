bad6da76664457d5ae163ee5206b05c0c57be384
Artur Shakhbazyan <artur_master13@mail.ru>
2024-02-20 17:06:24 +0300

    print result / functions

--- main.js
+++ main.js
@@ -14,14 +14,27 @@ btnMinus.onclick = function() {
   action = '-'
 }
 
+function printResult(result){
+  if (result < 0){
+    resultElement.style.color = 'red';
+  } else {
+    resultElement.style.color = 'green';
+  }
+  resultElement.textContent = result;
+}
 
+function computeNumbersWithAction(inp1, inp2, actionSymbol){
+  const num1 = Number(inp1.value);
+  const num2 = Number(inp2.value);
+  if (actionSymbol == '+'){
+    return(num1 + num2);
+  }
+  if (actionSymbol == '-'){
+    return(num1 - num2);
+  }
+}
 
 resultBtn.onclick = function(){
-  if (action == '+'){
-    const sum = Number(input1.value) + Number(input2.value);
-    resultElement.textContent = sum;
-  } else {
-    const sum = Number(input1.value) - Number(input2.value);
-    resultElement.textContent = sum;
-  }
+  const result = computeNumbersWithAction(input1, input2, action);
+  printResult(result);
 }
